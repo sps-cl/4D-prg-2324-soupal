ee505d4d1d26269d71078bb5a853fe0e27f7769b
4.D Šoupal Jan <Jan.Soupal@student.sps-cl.cz>
2023-09-18 14:32:59 +0200

    Vole

---
+++ 231909 program/StrategyPattern/StrategyPattern/operations/OperationMultiply.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern.operations
+{
+    internal class OperationMultiply
+    {
+        public int execute (int a, int b) 
+        {
+            return a * b;
+        } 
+    }
+}
